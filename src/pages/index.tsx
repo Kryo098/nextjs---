01471f7d37d57e6923d646ed5200e5b5e5e8e06d
Kryo098 <ryo0812eng@gmail.com>
2022-11-12 21:47:06 +0900

    ディレクトリ調整

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import { Photo } from '../pages/app'
+import { Photo } from './app'
 
 export default function Home() {
   return (
